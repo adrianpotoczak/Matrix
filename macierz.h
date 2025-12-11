54f484ba209929c63f02e58bb033f062119a4c91
Ja <adrian.potoczak@wp.pl>
2025-12-11 21:15:01 +0100

    Dodano nowy plik nagłówkowy macierz.h z początkową deklaracją klasy macierz.

---
+++ macierz.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <memory>   
+#include <vector>
+#include <algorithm>
+#include <stdexcept>
+#include <iomanip>
+
+class macierz {
+private:
+    int n;
+    std::unique_ptr<int[]> dane;
+    int indeks(int x, int y) const;
+
+public:
+
+};
